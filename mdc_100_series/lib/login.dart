fb2e7dbfcfe1e81b2146820839405de5603bba2c
haeyun1 <jhy041020@handong.ac.kr>
2024-11-05 10:38:05 +0900

    jhy041020@gmail.com firebase 계정으로 로그인 후 configure

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -28,14 +28,6 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 120.0),
             TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
               controller: _passwordController,
               decoration: const InputDecoration(
                 filled: true,
